65247a94d0066718e41ea4a6d5db0e1a057c2828
andrewwong-kwcml <andrewwong@kwcml.com>
2025-11-25 14:42:17 +0800

    schedule parameter
    1. daily run as one day before
    2. day 15 of current month to update previous whole month

--- SAP_Download_TransactionData/SAP/SAPHelper.cs
+++ SAP_Download_TransactionData/SAP/SAPHelper.cs
@@ -27,19 +27,48 @@ namespace SAP_Download_TransactionData
                     var command = new SapCommand("ZBAPI_GET_KWC_DN", connection);
                     //command.Parameters.Add("INIT_LOAD", ConfigurationManager.AppSettings["InitialLoad"]);
 
+                    List<SapComplexStructureParameter> complex = new List<SapComplexStructureParameter>();
+                    var p = new SapComplexStructureParameter();
+                    p.Structure = "BLDAT_RAN";
+                    p.Parameters.Add(new SapParameter("SIGN", "I"));
+                    p.Parameters.Add(new SapParameter("OPTION", "BT"));
+
                     if (ConfigurationManager.AppSettings["Filter"] == "X")
                     {
-                        List<SapComplexStructureParameter> complex = new List<SapComplexStructureParameter>();
-                        var p = new SapComplexStructureParameter();
-                        p.Structure = "BLDAT_RAN";
-                        p.Parameters.Add(new SapParameter("SIGN", "I"));
-                        p.Parameters.Add(new SapParameter("OPTION", "BT"));
                         p.Parameters.Add(new SapParameter("LOW", Convert.ToDateTime(ConfigurationManager.AppSettings["From"]).Date));
                         p.Parameters.Add(new SapParameter("HIGH", Convert.ToDateTime(ConfigurationManager.AppSettings["To"]).Date));
-                        complex.Add(p);
-                        command.ComplexParameters.Add("DOC_DATE_RANGE", complex);
+
+                        Console.WriteLine(string.Format("fromdate={0};todate={1}", ConfigurationManager.AppSettings["From"], ConfigurationManager.AppSettings["To"]));
+                        log.Debug(string.Format("fromdate={0};todate={1}", ConfigurationManager.AppSettings["From"], ConfigurationManager.AppSettings["To"]));
+                    }
+                    else
+                    {
+                        DateTime FirstDayLastMonth = new DateTime(DateTime.Now.Year, DateTime.Now.AddMonths(-1).Month, 1); ;
+                        DateTime LastDayLastMonth = new DateTime(FirstDayLastMonth.Year, FirstDayLastMonth.Month, DateTime.DaysInMonth(FirstDayLastMonth.Year, FirstDayLastMonth.Month));
+
+                        DateTime DayBefore = new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day);
+
+
+                        if (DateTime.Now.Day == 15)
+                        {
+                            p.Parameters.Add(new SapParameter("LOW", FirstDayLastMonth.Date));
+                            p.Parameters.Add(new SapParameter("HIGH", DayBefore.Date));
+
+                            Console.WriteLine(string.Format("fromdate={0};todate={1}", FirstDayLastMonth, DayBefore));
+                            log.Debug(string.Format("fromdate={0};todate={1}", FirstDayLastMonth, DayBefore));
+                        }
+                        else
+                        {
+                            p.Parameters.Add(new SapParameter("LOW", DayBefore.Date));
+                            p.Parameters.Add(new SapParameter("HIGH", DayBefore.Date));
+
+                            Console.WriteLine(string.Format("fromdate={0};todate={1}", DayBefore, DayBefore));
+                            log.Debug(string.Format("fromdate={0};todate={1}", DayBefore, DayBefore));
+                        }
                     }
 
+                    complex.Add(p);
+                    command.ComplexParameters.Add("DOC_DATE_RANGE", complex);
 
                     var sapDataReader = command.ExecuteDataSet();
 
